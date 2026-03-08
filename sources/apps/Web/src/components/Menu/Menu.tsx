6556da0c169e36ffd6fbe6c108c42136eb8e439a
Mikołaj Kuszowski <mikolajkuszowski@gmail.com>
2026-03-08 14:46:30 +0100

    Frontend home page improvements

--- sources/apps/Web/src/components/Menu/Menu.tsx
+++ sources/apps/Web/src/components/Menu/Menu.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from './Menu.module.css'
+import aghLogo from "../../assets/agh.svg";
 
 export function Menu() {
   return (
     <div className={styles.container}>
+      <img src={aghLogo} alt="AGH logo" className={styles.aghLogo}></img>
       <nav className={styles.menu}>
         <Link to="/">Start</Link>
-        <Link to="/robot-status">Status robota</Link>
         <Link to="/order-page">Zamówienia</Link>
+        <Link to="/robot-status">Status robota</Link>
       </nav>
     </div>
   );
--- sources/apps/Web/src/pages/HomePage/HomePage.tsx
+++ sources/apps/Web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import styles from './HomePage.module.css'
 import { API_URL } from "../../api/const";
+import { Link } from "react-router-dom";
 
 const HOME_API_URL = API_URL + "api/home";
 
@@ -16,8 +17,26 @@ export default function HomePage() {
 
   return (
     <div className={styles.container}>
-      <p>Page1</p>
-      <p>Pobrane dane: {data}</p>
+      <h2>Autonomiczny system kompletacji zamówień</h2>
+      <div className={styles.routeContainer}>
+        <p>Jeśli chcesz rozpocząć zamawianie kliknij&nbsp;</p>
+        <Link to="/order-page">Zamówienia</Link>
+      </div>
+      <div className={styles.routeContainer}>
+        <p>Jeśli chcesz sprawdzić status rozpoczętego zamówienia kliknij&nbsp;</p>
+        <Link to="/robot-status">Status robota</Link>
+      </div>
+      <div className={styles.apiStatusContainer}>
+        <div className={styles.apiStatusConfirmation}>
+          <p>Czy nawiązano połączenie z API?&nbsp;</p>
+          {data == "" ? (
+            <p className={styles.denialText}>NIE</p>
+          ) : (
+              <p className={styles.confirmationText}>TAK</p>
+          )}
+        </div>
+        {data == "" ? "" : <p>Pobrane dane: {data}</p>}
+      </div>
     </div>
   );
 }
